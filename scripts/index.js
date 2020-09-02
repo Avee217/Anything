c93a7b8e5159a8e6e85960457c65c213f39ef5f6
Avee217 <avee217@gmail.com>
2020-09-02 19:27:35 +0530

    fire starter

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,12 @@
 
-function notification() {
-    console.log("button was clicked");
+function lightFire() {
+
+    const newItem = document.createElement("li");
+    const textnode = document.createTextNode("The Fire has started");
+    newItem.appendChild(textnode);
+
+    const notification_list = document.getElementById("notification");
+
+    notification_list.insertBefore(newItem, notification_list.childNodes[0]);
+
 }
