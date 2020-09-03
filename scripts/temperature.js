55b6d2eade2233946cf1abcacd3cce59283fe957
Eyad Kobatte <ekobatte96@gmail.com>
2020-09-03 20:28:54 +0530

    fix conflict

--- scripts/temperature.js
+++ scripts/temperature.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function getTemperatureState(temperature) {
   if (temperature <= 29 && temperature >= 0) {
     return "freezing";
@@ -13,20 +12,3 @@ function getTemperatureState(temperature) {
     return "warm";
   }
 }
-=======
-
-export function getTemperatureState(temperature) {
-    if (temperature <= 29 && temperature >= 0) {
-        return 'freezing';
-    }
-    if (temperature <= 59 && temperature >= 30) {
-        return 'survivable';
-    }
-    if (temperature <= 89 && temperature >= 60) {
-        return 'pleasant';
-    }
-    if (temperature >= 90) {
-        return 'warm';
-    }
-}
->>>>>>> 0d660f1f21eeb6a23cb46a882e70bac8fe264d78
